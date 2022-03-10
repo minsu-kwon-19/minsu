55509915dc9119a3c4c37122876e9d4edde61197
권민수 <minsuft19@gmail.com>
2022-03-10 01:07:13 +0000

    add searching gu name fuction

--- RealEstate.py
+++ RealEstate.py
@@ -123,117 +123,123 @@ class RealEstateInfo(NamedTuple):
 #
 #       Main
 #
+headers = {'User-Agent': "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; Browzar)"}
 keyword1 = "송파구"
-keyword2 = "잠실동"
 excelFilePath = "./시세표 송파구.xlsm"
 
-url = "https://m.land.naver.com/search/result/" + keyword1 + keyword2
-headers = {'User-Agent': "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; Browzar)"}
+# 구에서 동 cortano 얻어오기.
+guUrl = "https://m.land.naver.com/search/result/" + keyword1    # 구단위
+resGuArea = getRes(guUrl, headers)
+strGuResult = getStrBetweenAnB(resGuArea.text, "filter: {", "},")
+guCortarNo = getStrBetweenAnB(strGuResult, "cortarNo: '","',")
 
-#prt = requests.get(url).text
-#print(prt)
-resArea = getRes(url, headers)
+# 해당 구의 행정동 리스트 뽑아오기.
+dongUrl = f"https://m.land.naver.com/map/getRegionList?cortarNo={guCortarNo}&mycortarNo={guCortarNo}"
+resDong = getRes(dongUrl, headers)
+dongJsonObject = json.loads(resDong.text)
+dongArray = dongJsonObject["result"]["list"]
 
-strResult = getStrBetweenAnB(resArea.text, "filter: {", "},")
+for dong in dongArray:
+    keyword2 = dong['CortarNm']    # 송파동, 잠실동 등.. 동 이름 keyword
 
-lat = getStrBetweenAnB(strResult, "lat: '","',")
-lon = getStrBetweenAnB(strResult, "lon: '","',")
-z = getStrBetweenAnB(strResult, "z: '","',")
-cortarNo = getStrBetweenAnB(strResult, "cortarNo: '","',")
-searchType = "APT"
+    url = "https://m.land.naver.com/search/result/" + keyword1 + keyword2
 
-rletTpCds = getStrBetweenAnB(strResult, "rletTpCds: '","',")
-tradTpCds = getStrBetweenAnB(strResult, "tradTpCds: '","',")
+    resArea = getRes(url, headers)
 
-# 지도에서 단지들 url
-onMapURL = f"https://m.land.naver.com/cluster/clusterList?cortarNo={cortarNo}&rletTpCd={searchType}&tradTpCd=A1%3AB1%3AB2&z={z}&lat={lat}&lon={lon}&btm=37.4756089&lft=127.099033&addon=COMPLEX&bAddon=COMPLEX&isOnlyIsale=false"
+    strResult = getStrBetweenAnB(resArea.text, "filter: {", "},")
 
-resMap = getRes(onMapURL, headers)
+    lat = getStrBetweenAnB(strResult, "lat: '","',")
+    lon = getStrBetweenAnB(strResult, "lon: '","',")
+    z = getStrBetweenAnB(strResult, "z: '","',")
+    cortarNo = getStrBetweenAnB(strResult, "cortarNo: '","',")
+    searchType = "APT"
 
-mapJsonObject = json.loads(resMap.text)
-mapArray = mapJsonObject["data"]["COMPLEX"]
+    rletTpCds = getStrBetweenAnB(strResult, "rletTpCds: '","',")
+    tradTpCds = getStrBetweenAnB(strResult, "tradTpCds: '","',")
 
-filepath = excelFilePath #workbook 객체 생성
+    # 지도에서 단지들 url
+    onMapURL = f"https://m.land.naver.com/cluster/clusterList?cortarNo={cortarNo}&rletTpCd={searchType}&tradTpCd=A1%3AB1%3AB2&z={z}&lat={lat}&lon={lon}&btm=37.4756089&lft=127.099033&addon=COMPLEX&bAddon=COMPLEX&isOnlyIsale=false"
 
-wb = op.load_workbook(filepath, data_only = 'True') #worksheet 객체 생성
+    resMap = getRes(onMapURL, headers)
 
-ws = wb[f"{keyword2}"]
+    mapJsonObject = json.loads(resMap.text)
+    mapArray = mapJsonObject["data"]["COMPLEX"]
 
-print(f"workSheet name : {ws}")
+    wb = op.load_workbook(excelFilePath, data_only = 'True', read_only=False, keep_vba=True) #worksheet 객체 생성
 
-# 매물 정보
-for val in mapArray:
-    # print(f'{val["lgeo"]}, {val["lat"]}, {val["lon"]}, {val["count"]}')
-    
-    # 매물 url
-    # 20개 이상일땐 page 0부터시작
-    # 20개 이하일땐 page 1부터 시작
-    ipage = int(int(val['count'])/20) # 20개씩으로 끊어짐.
-    if int(int(val['count'])/20) == 0:
-        ipage = ipage + 2    
-    else:
-        ipage = ipage + 1
-        
+    ws = wb[f"{keyword2}"]
 
+    print(f"workSheet name : {ws}")
 
-    # 각 지도에서 20개의 매물이 합쳐져 보이는거 체크.
-    for pageCnt in range(1, ipage):
-        # 매물 20개가 넘는 위치들 카운트 하기 위함.
-        offeringsURL = f"https://m.land.naver.com/cluster/ajax/complexList?itemId={val['lgeo']}&lgeo={val['lgeo']}&rletTpCd=APT&tradTpCd=A1:B1:B2&z={z}&lat={val['lat']}&lon={val['lon']}&btm=37.4404697&lft=127.1156627&top=37.5303033&rgt=127.1285373&cortarNo={cortarNo}&isOnlyIsale=false&sort=readRank&page={pageCnt}"
-        resOfferings = getRes(offeringsURL, headers)
+    # 매물 정보
+    for val in mapArray:
+        # 매물 url
+        # 20개 이상일땐 page 0부터시작
+        # 20개 이하일땐 page 1부터 시작
+        ipage = int(int(val['count'])/20) # 20개씩으로 끊어짐.
+        if int(int(val['count'])/20) == 0:
+            ipage = ipage + 2    
+        else:
+            ipage = ipage + 1
 
-        offeringsJsonObject = json.loads(resOfferings.text)
-        offeringsArray = offeringsJsonObject["result"]
+        # 각 지도에서 20개의 매물이 합쳐져 보이는거 체크.
+        for pageCnt in range(1, ipage):
+            # 매물 20개가 넘는 위치들 카운트 하기 위함.
+            offeringsURL = f"https://m.land.naver.com/cluster/ajax/complexList?itemId={val['lgeo']}&lgeo={val['lgeo']}&rletTpCd=APT&tradTpCd=A1:B1:B2&z={z}&lat={val['lat']}&lon={val['lon']}&btm=37.4404697&lft=127.1156627&top=37.5303033&rgt=127.1285373&cortarNo={cortarNo}&isOnlyIsale=false&sort=readRank&page={pageCnt}"
+            resOfferings = getRes(offeringsURL, headers)
 
-        # 지역 전체 매물 array
-        for v in offeringsArray:
-            if int(v['totHsehCnt']) > 300:
-                print(f"{v['hscpNm']}, {v['dealCnt']}, {v['leaseCnt']}")
+            offeringsJsonObject = json.loads(resOfferings.text)
+            offeringsArray = offeringsJsonObject["result"]
 
-                # 아파트 매물 url
-                num = int(v['dealCnt']) + int(v['leaseCnt'])                
-                aptPage =  int(num / 20) + 2
+            # 지역 전체 매물 array
+            for v in offeringsArray:
+                if int(v['totHsehCnt']) > 300:
+                    print(f"{v['hscpNm']}, {v['dealCnt']}, {v['leaseCnt']}")
 
-                myDealList = []
-                myLeaseList = []
-                
-                # 매매, 전세 페이지 처리. # 20개 이상일 때 page 증가됨.
-                for aptCnt in range(1, aptPage):
-                    aptUrl = f'https://m.land.naver.com/complex/getComplexArticleList?hscpNo={v["hscpNo"]}&cortarNo=1171010800&tradTpCd=A1:B1&order=point_&showR0=N&page={aptCnt}'
-                    resApt = getRes(aptUrl, headers)
-                    
-                    aptJsonObject = json.loads(resApt.text)
-                    aptArray = aptJsonObject['result']['list']
-                    
-                    # APT 단지 매물 검색
-                    # 층수 "flrInfo":"11/15" "저/15", "고/15"
-                    # 공급/전용 "spc1":"103.12","spc2":"84.7"
-                    # 매매와 전세 리스트 따로 저장하기.
-                    for apt in aptArray:  
-                        if apt['tradTpNm'] == "매매":
-                            removeLowFloor(apt, myDealList)
-                        elif apt['tradTpNm'] == "전세":
-                            removeLowFloor(apt, myLeaseList)
+                    # 아파트 매물 url
+                    num = int(v['dealCnt']) + int(v['leaseCnt'])                
+                    aptPage =  int(num / 20) + 2
 
-                # 매매와 전세가격중 최소값만 저장.
-                # 엑셀 데이터에 입력해야함.
-                myDealList = getMinVal(myDealList)
-                myLeaseList = getMinVal(myLeaseList)
-                        
-                # input data.
-                for cnt in range(6, ws.max_row + 1):    # sheet range
-                    for dealApt in myDealList:  # APT range
-                        if ws[f'A{cnt}'].value == dealApt.name and ws[f'D{cnt}'].value == round(dealApt.spc1, 1) and ws[f'F{cnt}'].value == round(dealApt.spc2,1):
-                            ws[f'I{cnt}'] = str(dealApt.priceInfo)
-                            ws[f'J{cnt}'] = str(dealApt.floorInfo)
+                    myDealList = []
+                    myLeaseList = []
 
-                    for leaseApt in myLeaseList:
-                        if ws[f'A{cnt}'].value == leaseApt.name and ws[f'D{cnt}'].value == round(leaseApt.spc1, 1) and ws[f'F{cnt}'].value == round(leaseApt.spc2, 1):
-                            ws[f'K{cnt}'] = str(leaseApt.priceInfo)
-                            ws[f'L{cnt}'] = str(leaseApt.floorInfo)
+                    # 매매, 전세 페이지 처리. # 20개 이상일 때 page 증가됨.
+                    for aptCnt in range(1, aptPage):
+                        aptUrl = f'https://m.land.naver.com/complex/getComplexArticleList?hscpNo={v["hscpNo"]}&cortarNo=1171010800&tradTpCd=A1:B1&order=point_&showR0=N&page={aptCnt}'
+                        resApt = getRes(aptUrl, headers)
 
-                # save
-                wb.save("./시세표 송파구.xlsx")
+                        aptJsonObject = json.loads(resApt.text)
+                        aptArray = aptJsonObject['result']['list']
+
+                        # APT 단지 매물 검색
+                        # 층수 "flrInfo":"11/15" "저/15", "고/15"
+                        # 공급/전용 "spc1":"103.12","spc2":"84.7"
+                        # 매매와 전세 리스트 따로 저장하기.
+                        for apt in aptArray:  
+                            if apt['tradTpNm'] == "매매":
+                                removeLowFloor(apt, myDealList)
+                            elif apt['tradTpNm'] == "전세":
+                                removeLowFloor(apt, myLeaseList)
+
+                    # 매매와 전세가격중 최소값만 저장.
+                    # 엑셀 데이터에 입력해야함.
+                    myDealList = getMinVal(myDealList)
+                    myLeaseList = getMinVal(myLeaseList)
+
+                    # input data.
+                    for cnt in range(6, ws.max_row + 1):    # sheet range
+                        for dealApt in myDealList:  # APT range
+                            if ws[f'A{cnt}'].value == dealApt.name and ws[f'D{cnt}'].value == round(dealApt.spc1, 1) and ws[f'F{cnt}'].value == round(dealApt.spc2,1):
+                                ws[f'I{cnt}'] = str(dealApt.priceInfo)
+                                ws[f'J{cnt}'] = str(dealApt.floorInfo)
+
+                        for leaseApt in myLeaseList:
+                            if ws[f'A{cnt}'].value == leaseApt.name and ws[f'D{cnt}'].value == round(leaseApt.spc1, 1) and ws[f'F{cnt}'].value == round(leaseApt.spc2, 1):
+                                ws[f'K{cnt}'] = str(leaseApt.priceInfo)
+                                ws[f'L{cnt}'] = str(leaseApt.floorInfo)
+
+                    # save
+                    wb.save(f"{excelFilePath}")
 
 
                         
